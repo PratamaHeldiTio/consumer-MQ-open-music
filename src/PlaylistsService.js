6387f6f41f1576b6498f6701d07353a347ba94de
pratamahelditio <pratamahelditio@gmail.com>
2021-10-17 21:13:54 +0700

    feat: add consumer for openmusic

---
+++ src/PlaylistsService.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getSongsOnPlaylist(playlistId) {
+    const query = {
+      text: `
+      SELECT * FROM songs
+      LEFT JOIN playlistsongs ON playlistsongs.song_id = songs.id
+      WHERE playlistsongs.playlist_id = $1`,
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
+}
+
+module.exports = PlaylistsService;
